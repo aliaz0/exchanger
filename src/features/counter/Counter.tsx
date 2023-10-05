fd25ea8604423803914c47e5ca6d8a41fa14e2ef
Ali <ali.azkia@gmail.com>
2023-10-05 16:27:51 +0330

    Use BEM naming method

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -9,7 +9,7 @@ import {
   incrementIfOdd,
   selectCount,
 } from "./counterSlice"
-import styles from "./Counter.module.css"
+import "./Counter.css"
 
 export function Counter() {
   const count = useAppSelector(selectCount)
@@ -20,44 +20,44 @@ export function Counter() {
 
   return (
     <div>
-      <div className={styles.row}>
+      <div className="flex-row">
         <button
-          className={styles.button}
+          className="flex-row__child button"
           aria-label="Decrement value"
           onClick={() => dispatch(decrement())}
         >
           -
         </button>
-        <span className={styles.value}>{count}</span>
+        <span className="flex-row__child label">{count}</span>
         <button
-          className={styles.button}
+          className="flex-row__child button"
           aria-label="Increment value"
           onClick={() => dispatch(increment())}
         >
           +
         </button>
       </div>
-      <div className={styles.row}>
+      <div className="flex-row">
         <input
-          className={styles.textbox}
+          className="flex-row__child textbox"
           aria-label="Set increment amount"
           value={incrementAmount}
           onChange={(e) => setIncrementAmount(e.target.value)}
         />
         <button
-          className={styles.button}
+          className="flex-row__child button"
           onClick={() => dispatch(incrementByAmount(incrementValue))}
         >
           Add Amount
         </button>
         <button
-          className={styles.asyncButton}
+          className="flex-row__child button button--async"
           onClick={() => dispatch(incrementAsync(incrementValue))}
         >
           Add Async
         </button>
         <button
-          className={styles.button}
+          className="flex-row__child button"
           onClick={() => dispatch(incrementIfOdd(incrementValue))}
         >
           Add If Odd
